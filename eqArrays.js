10b71cfafd702c693fbcd801113e1e213838e60e
Michael Law <mikelaw86@hotmail.com>
2022-03-09 21:34:04 -0500

    Add new lotide functions

---
+++ eqArrays.js
@@ -0,0 +1,19 @@
+const eqArrays = function(actual, expected) {
+  if (actual.length !== expected.length) {
+    return false
+  }
+ 
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      return false
+    }
+  }
+  return true;
+};
+
+
+// test cases
+console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
---
+++ without.js
@@ -0,0 +1,39 @@
+const eqArrays = function(actual, expected) {
+  if (actual.length !== expected.length) {
+    return false;
+  }
+
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`👍👍👍 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😒😒😒 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const without = function(source, itemsToRemove) {
+  let result = []
+  for (let i = 0; i < source.length; i++) {
+    console.log(itemsToRemove.includes(source[i]))
+      if (!itemsToRemove.includes(source[i])) {
+        result.push(source[i]);
+    }  
+  }
+  return result
+}
+
+assertArraysEqual(without([1, 2, 3], [1]), [2, 3])
+assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"])
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
