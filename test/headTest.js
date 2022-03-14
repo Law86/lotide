eb65f5abe86b5522b1662864ab35ae2b02854b5f
Michael Law <mikelaw86@hotmail.com>
2022-03-14 12:53:19 -0400

    Add refactor of head tests to use M&C

--- test/headTest.js
+++ test/headTest.js
@@ -1,6 +1,14 @@
 const head = require('../head');
 const assertEqual = require('../assertEqual');
 
-// TEST CASES
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assertEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assertEqual(head(['5']), '5'); 
+  });
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']",  () => {
+    assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); 
+  });
+});
