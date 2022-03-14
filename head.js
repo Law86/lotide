4e62aa81965f1cdd9d0bfbc912e281e5739766a4
Michael Law <mikelaw86@hotmail.com>
2022-03-14 13:53:21 -0400

    Added fixes to overall function

--- head.js
+++ head.js
@@ -1,11 +1,9 @@
-const assertEqual = require('./assertEqual'); 
-
 const head = function(arr1) {
   if (Array.isArray(arr1)) {
     if (arr1.length > 0) {
       return(arr1[0]);
     } else {
-      return("Undefined");
+      return null;
     }
   }
 }
