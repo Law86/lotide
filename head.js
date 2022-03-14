78e93f4470b0877f13d87ef3fcebd8f2a2fcd495
Michael Law <mikelaw86@hotmail.com>
2022-03-14 07:52:26 -0400

    Add refactor head and assertEqual w/ modules

--- head.js
+++ head.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`👍👍👍 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`😒😒😒 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual'); 
 
 const head = function(arr1) {
   if (Array.isArray(arr1)) {
@@ -14,7 +8,6 @@ const head = function(arr1) {
       console.log("Undefined");
     }
   }
-};
+}
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
