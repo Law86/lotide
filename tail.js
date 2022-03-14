2d22aa33ef4907f741fb3a0880b82b217c802e8c
Michael Law <mikelaw86@hotmail.com>
2022-03-14 08:02:03 -0400

    Add refactor of tail w module

--- tail.js
+++ tail.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`👍👍👍 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`😒😒😒 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual'); 
 
 const tail = function(arr1) {
   if (Array.isArray(arr1)) {
@@ -14,6 +8,4 @@ const tail = function(arr1) {
   }
 };
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+module.exports = tail;
