b1feed02f898263c58ef3f539fcfe62d07561250
Michael Law <mikelaw86@hotmail.com>
2022-03-14 13:50:29 -0400

    Added test file for tail

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,15 @@
 const tail = require('../tail');
-const assertEqual = require('../assertEqual');
-
+const assert = require('chai').assert;
 const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+
+describe("#tail", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(tail([1, 2, 3]), 3);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(tail(['5']), '5'); 
+  });
+  it("returns 'Labs' for ['Hello', 'Lighthouse', 'Labs']",  () => {
+    assert.strictEqual(tail(["Hello", "Lighthouse", "Labs"]), "Labs"); 
+  });
+});
